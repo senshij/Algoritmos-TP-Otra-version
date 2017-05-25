3fa074b888f5e072099c2106df96582e4f010119
Juan <juammpa@gmail.com>
2017-05-25 16:11:54 -0300

    Add files via upload

--- errors.c
+++ errors.c
@@ -2,12 +2,6 @@
 #include "types.h"
 #include "config.h"
 #include "errors.h"
-#define MSG_ERROR_INVALID_DATA "El dato leído es inválido."
-#define MSG_ERROR_EOF "Fin de archivo."
-#define MSG_ERROR_INVALID_ARGS "Los argumentos ingresados son inválidos."
-#define MSG_ERROR_INVALID_NUMBER_ARGS "El número de argumentos ingresados es incorrecto."
-#define MSG_ERROR_NULL_POINTER "Error del sistema."
-
 
 
 void print_error_message(status_t st){
